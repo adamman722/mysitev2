ff46e7caeb826105e8f1879977ec6464f30d5f18
adamman722 <tarpeyadam@gmail.com>
2022-05-21 18:47:37 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import LandingPage from "./components/landingComponent/LandingPage";
+import AboutMe from "./components/aboutme/AboutMe";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<LandingPage />}>
+          <Route path="/about" element={<AboutMe />} />
+          {/* <Route path="projects" element={} />
+        <Route path="contact" element={} /> */}
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/aboutme/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+function AboutMe() {
+  return (
+    <div>
+      AboutMe
+      <Link to={`/`}>back home</Link>
+    </div>
+  );
+}
+
+export default AboutMe;
